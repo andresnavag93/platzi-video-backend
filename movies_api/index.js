e0cfd63cea266ba9a3aaeb05107b853a4a1fff51
Andres Navarro <andresnavag93@gmail.com>
2020-12-22 17:36:47 -0400

    :goal_net: Validations with boom

--- movies_api/index.js
+++ movies_api/index.js
@@ -2,19 +2,28 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-const moviesApi = require('./routes/movies');
+const moviesApi = require('./routes/movies.js');
 
 const {
   logErrors,
+  wrapErrors,
   errorHandler,
 } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
 
+// routes
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function () {
