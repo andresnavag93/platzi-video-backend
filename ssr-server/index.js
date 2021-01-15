4ba5c51cba4ecce1a9a5f49b4ac5bccefbac38c6
Andres Navarro <andresnavag93@gmail.com>
2021-01-15 08:17:08 -0400

    :hammer: Implementing cookies rememberMe

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -15,7 +15,14 @@ app.use(cookieParser());
 //  Basic strategy
 require('./utils/auth/strategies/basic');
 
+// Time variables
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
+
 app.post('/auth/sign-in', async function (req, res, next) {
+  // We obtain attribute rememberMe in the body request
+  const { rememberMe } = req.body;
+
   passport.authenticate('basic', function (error, data) {
     try {
       if (error || !data) {
@@ -27,9 +34,12 @@ app.post('/auth/sign-in', async function (req, res, next) {
           next(error);
         }
 
+        const { token, ...user } = data;
+
         res.cookie('token', token, {
           httpOnly: !config.dev,
           secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
         });
 
         res.status(200).json(user);
